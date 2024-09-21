1670b1f5fcac6be989b6601fafc89d2d42a3bf09
lekhyaruhi <lekhyasree555@gmail.com>
2024-09-21 11:51:59 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function beforesubmit(){
+
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value",inputdate.value);
+
+ let formattedDate = new Date (inputdate.value).toLocaleDateString("en-IN");
+ console.log("formattedDate");
+ outputdate.value  = formattedDate;
+ console.log("outputdate.value");
+}
