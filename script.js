a7d8094879327d7dacf533eff1c2a19512cf44a0
lekhyaruhi <lekhyasree555@gmail.com>
2024-09-21 13:37:18 +1000

    recaptcha changes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function beforesubmit(){
-
+let captchachecked = false;
+function beforesubmit(event){
+if(captchachecked){
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value",inputdate.value);
-
+}
+else{
+    alert("please check the recaptcha");
+    event.preventDefault(); //stop submission of the form//
+}
  let formattedDate = new Date (inputdate.value).toLocaleDateString("en-IN");
  console.log("formattedDate");
  outputdate.value  = formattedDate;
  console.log("outputdate.value");
 }
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500)
+    ;
+function captchasuccess(){
+    captchachecked ="true";
+}
